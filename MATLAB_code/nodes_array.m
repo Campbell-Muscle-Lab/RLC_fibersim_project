a85d7509c06f55ebe07f2f04dfbe991616a418d7
CaterinaSquarci <csq222@uky.edu>
2024-10-28 18:21:42 -0400

    update

---
+++ MATLAB_code/nodes_array.m
@@ -0,0 +1,16 @@
+function nodes_array 
+% Function create an array for the nodes in FiberSim 
+
+% Code
+
+v = [1;2]
+v1 = [1;1]
+
+a = repmat(v, 10, 1)'
+b = repmat(v1,10,1)'
+
+array = [a,b]
+
+
+ 
+
