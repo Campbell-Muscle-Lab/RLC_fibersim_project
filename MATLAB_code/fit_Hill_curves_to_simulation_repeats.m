9ff63c3eab0d259b59ad13afa8b44cbfde162bc1
Ken Campbell <8246018+kenatcampbellmusclelab@users.noreply.github.com>
2024-11-27 13:21:34 -0500

    Ken added fit_Hill_curves_to_simulation_repeats

---
+++ MATLAB_code/fit_Hill_curves_to_simulation_repeats.m
@@ -0,0 +1,75 @@
+function fit_Hill_curves_to_simulation_repeats
+% Takes a standard FiberSim pCa_analysis file and calculates fits for each
+% repeat
+
+%Path
+addpath(genpath('../../MATLAB_Utilities'))
+
+% Variables 
+pCa_analysis_file = '../test_data/pCa_analysis.xlsx';
+output_file = '../test_data/Hill_curve_repeats.xlsx';
+
+% Read excel sheet 
+d = readtable(pCa_analysis_file)
+
+dn = d.Properties.VariableNames'
+
+% Find the number of conditions and pCa values to analyze
+unique_conditions = unique(d.curve);
+unique_pCa_values = unique(d.hs_pCa);
+
+% Deduce number of repeats
+no_of_repeats = size(d,1) / ...
+    (numel(unique_conditions) * numel(unique_pCa_values))
+
+% Create a variable to hold the output
+out = [];
+
+output_i = 1;
+
+% Loop through the conditions
+for cond_i = 1 : numel(unique_conditions)
+
+    % Find all the indices for the condition
+    vi_cond = find(d.curve == unique_conditions(cond_i));
+
+    % Now pull off the values for the different repeats
+    for repeat_i = 1 : no_of_repeats
+
+        vi_repeat = vi_cond(repeat_i : no_of_repeats : numel(vi_cond))
+
+        pCa = d.hs_pCa(vi_repeat);
+        y = d.hs_force(vi_repeat);
+
+        % Fit the curve
+        [pCa_50, n_H, f_min, f_amp, r_sq] = ...
+            fit_Hill_curve(pCa, y)
+    
+        % Store the data
+        out.curve(output_i) = unique_conditions(cond_i);
+        out.repeat(output_i) = repeat_i;
+        out.hs_length(output_i) = d.hs_length(vi_repeat(1));
+        out.pCa_50(output_i) = pCa_50;
+        out.n_H(output_i) = n_H;
+        out.f_min(output_i) = f_min;
+        out.f_amp(output_i) = f_amp;
+        out.f_max(output_i) = f_min + f_amp;
+        out.r_sq(output_i) = r_sq;
+    
+        % Increment the counter
+        output_i = output_i + 1;
+
+    end
+end
+
+% out is a structure, with data in wrong row column format
+out = columnize_structure(out);
+
+% Convert to a table
+out = struct2table(out)
+
+% Clean any existing file
+try
+    delete(output_file);
+end
+writetable(out, output_file);
--- MATLAB_code/plot_hill_coefficient.m
+++ MATLAB_code/plot_hill_coefficient.m
@@ -1,40 +1,19 @@
-function plot_hill_coefficient 
+function fit_Hill_curves_to_simulation_repeats
+% Takes a standard FiberSim pCa_analysis file and calculates fits for each
+% repeat
 
 %Path
 addpath(genpath('../../MATLAB_Utilities'))
 
 % Variables 
-xls_file = '../Simulations/specific_molecules/sim_data/sim_output/pCa_analysis.xlsx';
-output_image_file = '../output/pCa_curves_all_fil';
-output_image_types = {'png', 'svg', 'eps'};
-
-% Display 
- color_map = [0.8 0 1 ; 1 0.6 0 ; 0.8 0 1; 1 0.6 0; ...
-             1 0.7 0.3 ; 0.8 0 1; 0 0.8 0.8]
- f_scaling_factor = 0.001;
-y_label_offset = -0.05;
-title_y_offset = 0.1;
-x_label_offset = 0.1;
-buffer = 20 ;
-
-% Code 
-% Figure layout
-no_of_cols = 1
-no_of_rows = 1 
-
-%sp = initialise_publication_quality_figure(...
-    % 'no_of_panels_wide', no_of_cols,...
-    % 'no_of_panels_high', no_of_rows, ...
-    % 'x_to_y_axes_ratio', 1.3, ...
-    % 'right_margin', 1, ...
-    % 'axes_padding_top', 0 , ...
-    % 'axes_padding_bottom', 0.4, ...
-    % 'panel_label_font_size', 0, ...
-    % 'top_margin', 0.4, ...
-    % 'bottom_margin', 0.2);
+xlsx_file = '../test_data/pCa_analysis.xlsx';
 
 % Read excel sheet 
-d = readtable(xls_file) 
+d = readtable(xlsx_file)
+
+dn = d.Properties.VariableNames'
+
+return
 
 % Define needed files 
 curve_id = unique(d.curve)
